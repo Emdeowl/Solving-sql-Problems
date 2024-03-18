d0a30d015b9e820496ce2189d162a923cdb62233
Emdeowl <96479179+Emdeowl@users.noreply.github.com>
2024-03-18 15:39:06 +0900

    Add files via upload

---
+++ 93번 .sql
@@ -0,0 +1,14 @@
+# 1075. Project Employees I
+
+# Write your MySQL query statement below
+
+SELECT
+    P.project_id,
+    round(avg(E.experience_years),2) as average_years
+FROM
+    Project  AS P 
+JOIN 
+    Employee  AS E 
+ON P.employee_id  = E.employee_id 
+GROUP BY
+    P.project_id
