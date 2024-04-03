7a64ede741fdecea90b9d96b540d5a70e7234651
Emdeowl <96479179+Emdeowl@users.noreply.github.com>
2024-04-03 20:55:08 +0900

    문제를 찍어서 맞춤

---
+++ leetcode_129.sql
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+SELECT
+    DISTINCT ID,
+    NAME,
+    COUNTRYCODE,
+    DISTRICT,
+    POPULATION
+FROM
+    CITY
+--  문제를 몰라서 찍어서 맞춤 ㄷㄷ
