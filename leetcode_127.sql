e3a9777fda23e0bcf8f2f8072fd066d6edf6b22b
Emdeowl <96479179+Emdeowl@users.noreply.github.com>
2024-04-03 20:59:46 +0900

    130번

--- leetcode_127.sql
+++
@@ -1,8 +0,0 @@
--- https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
-select
-    *
-from
-    city
-where
-    population > 100000 and countrycode = 'USA'
-    
--- leetcode_128.sql
+++
@@ -1,7 +0,0 @@
--- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
-select
-    name
-from
-    city
-where
-    population > 120000 and countrycode ='USA'
