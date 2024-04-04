b4743e876ea94e5e01a580def4425f67f545580d
Emdeowl <96479179+Emdeowl@users.noreply.github.com>
2024-04-04 10:18:46 +0900

    134번

---
+++ leetcode_134.sql
@@ -0,0 +1,8 @@
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-2/problem?isFullScreen=true
+*/
+SELECT
+    ROUND(SUM(lat_N) ,2),
+    ROUND(SUM(LONG_W) ,2)
+FROM
+    Station
