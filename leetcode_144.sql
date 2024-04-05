ebd29dd68fc40fc130ffd303df9e55559b431e32
Emdeowl <96479179+Emdeowl@users.noreply.github.com>
2024-04-05 11:34:38 +0900

    144번

---
+++ leetcode_144.sql
@@ -0,0 +1,9 @@
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true
+*/
+SELECT
+    DISTINCT CITY
+FROM
+    STATION
+WHERE
+    (CITY   REGEXP '^[^aeiou]')  and ( CITY  REGEXP '[^aeiou]$')
