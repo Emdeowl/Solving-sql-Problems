533770414df74b84322a67d2040816edcde501d2
Emdeowl <96479179+Emdeowl@users.noreply.github.com>
2024-04-05 11:02:52 +0900

    141번

---
+++ leetcode_141.sql
@@ -0,0 +1,10 @@
+/*
+https://www.hackerrank.com/challenges/weather-observation-station-9/problem?isFullScreen=true
+*/
+
+SELECT
+    DISTINCT CITY
+FROM
+    STATION
+WHERE
+    CITY regexp '^[^a,e,i,o,u]'
