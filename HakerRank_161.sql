eb93c4abaa6a0aff841ceec0d821d2849dc7b42f
Emdeowl <96479179+Emdeowl@users.noreply.github.com>
2024-04-12 14:03:13 +0900

    161

---
+++ HakerRank_161.sql
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-16/problem?isFullScreen=true
+SELECT
+    ROUND(LAT_N,4)
+FROM
+    STATION
+WHERE
+    LAT_N  > 38.7780
+ORDER BY
+    1 asc
+LIMIT 1
